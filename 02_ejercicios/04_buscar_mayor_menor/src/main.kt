3fe58537842f25aaaf9a60a8f15b2f5a17cb7b38
Unknown <davidnunovelez@gmail.com>
2017-05-25 21:45:56 +0200

    ejercicios kotlin entre el 1 y el 4 terminados

--- 02_ejercicios/04_buscar_mayor_menor/src/main.kt
+++ 02_ejercicios/04_buscar_mayor_menor/src/main.kt
@@ -1,8 +1,52 @@
+import java.util.*
+
 /**
  * Created by 7fbd06 on 25/05/2017.
  */
 fun main(args: Array<String>) {
 
 
-    
+    fun int(min: Int, max: Int): Int = min + Random().nextInt((max - min) + 1)
+
+    val datos = arrayOf(int(0,100),int(0,100),int(0,100),int(0,100),int(0,100),
+            int(0,100),int(0,100),int(0,100),int(0,100),int(0,100))
+
+
+
+    fun visualizar(array: Array<Int>) {
+        for(i in array){
+            println(i)
+        }
+    }
+
+    fun buscarMayor(array: Array<Int>){
+            val max=array.max()
+            println("El valor máximo del array es: $max")
+    }
+
+    fun buscarMenor(array: Array<Int>){
+            val min=array.min()
+            println("El valor mínimo del array es: $min")
+    }
+
+    fun existe(array: Array<Int>, n:Int):Boolean{
+        var existe:Boolean=false
+        for(i in array){
+            if(i==n)
+                return true
+        }
+        return existe
+    }
+
+
+    visualizar(datos)
+    println()
+    buscarMayor(datos)
+    println()
+    buscarMenor(datos)
+    if(existe(datos, 20)==true){
+        println("Existe")
+    }else{
+        println("No existe")
+    }
 }
