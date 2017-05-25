e275a14145db3bf7bf51208dcd901bbb3607c7e3
Unknown <davidnunovelez@gmail.com>
2017-05-25 21:05:01 +0200

    ejercicios kotlin

--- 02_ejercicios/03_lista_primos/src/main.kt
+++ 02_ejercicios/03_lista_primos/src/main.kt
@@ -1,3 +1,5 @@
+import javax.swing.JOptionPane
+
 /**
  * Created by 7fbd06 on 25/05/2017.
  */
@@ -5,8 +7,29 @@
 fun main(args: Array<String>) {
 
 
-    fun esPrimo(n:Int): {
+    fun esPrimo(n:Int): Boolean{
+        var contador:Int=2
+        var primo: Boolean=true
+        while ((primo) && (contador!=n)){
+            if (n % contador == 0)
+                primo = false
+                contador += 1
 
+        }
+        return primo
     }
+
+    val numerousuario=JOptionPane.showInputDialog("Escribe un nº: ").toInt()
+
+    for (i in 1..numerousuario){
+        if(esPrimo(i)==true){
+            println(i)
+        }
+    }
+
+
+
+
+
 
 }
---
+++ 02_ejercicios/04_buscar_mayor_menor/src/main.kt
@@ -0,0 +1,8 @@
+/**
+ * Created by 7fbd06 on 25/05/2017.
+ */
+fun main(args: Array<String>) {
+
+
+    
+}
